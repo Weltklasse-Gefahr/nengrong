d9589953b5bdfab927c6cc021da9213f8c84490c
qianqiang <qianqiang@ctyun.cn>
2015-11-29 15:12:02 +0800

    修改密码

--- Home/Controller/UserController.class.php
+++ Home/Controller/UserController.class.php
@@ -12,95 +12,108 @@ class UserController extends Controller
 
     //登录
     public function login(){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $dynamicCode = $_POST['dynamicCode'];
-        $mDynamicCode = $_POST['mDynamicCode'];
-        if (empty($email) || empty($password) || empty($dynamicCode)) {
-            echo '{"code":"-1","msg":"邮箱或者密码或者动态码为空！"}';
-        }
-        if (!($mDynamicCode == MD5($dynamicCode."ENFENF"))) {
-            echo '{"code":"-1","msg":"验证码错误"}';
-        }
+        if($_POST['rtype'] == 1 || $_GET['rtype'] == 1){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $dynamicCode = $_POST['dynamicCode'];
+            $mDynamicCode = $_POST['mDynamicCode'];
+            if (empty($email) || empty($password) || empty($dynamicCode)) {
+                echo '{"code":"-1","msg":"邮箱或者密码或者动态码为空！"}';
+            }
+            if (!($mDynamicCode == MD5($dynamicCode."ENFENF"))) {
+                echo '{"code":"-1","msg":"验证码错误"}';
+            }
 
-        $user = D('User','Service');
-        $users = $user->loginService();
-        
-        $display = $_GET['display'];
-        if ($display == 'json') {
-            dump($users);
-            echo json_encode($users);
-            exit();
-        }
-        if (sizeof($users) == 1) {
-            echo '{"code":"0","msg":""}';
+            $user = D('User','Service');
+            $users = $user->loginService();
+            
+            if ($_GET['display'] == 'json') {
+                dump($users);
+                echo json_encode($users);
+                exit();
+            }
+            if (sizeof($users) == 1) {
+                echo '{"code":"0","msg":""}';
 
-        } else {
-            echo '{"code":"-1","msg":"登录信息错误"}';
+            } else {
+                echo '{"code":"-1","msg":"登录信息错误"}';
+            }
+            if($users->user_type == 1){
+                $this->display(index);
+            }else if($users->user_type == 2){
+                $this->display(index);
+            }else if($users->user_type == 3){
+                $this->display(index);
+            }else if($users->user_type == 4){
+                $this->display(index);
+            }
+        }else {
+            $this->display();
         }
-        $this->display();
     }
 
     //注册
     public function register(){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $userType = $_POST['userType'];
-        if (empty($email) || empty($password)) {
-            echo '{"code":"-1","msg":"邮箱或者密码为空！"}';
-        }
-        $user = M("User");
-        $users = $user->where("email='%s' and status!=9999", array($email) )->select();
-        if (sizeof($users) == 1) {
-            echo '{"code":"-1","msg":"该邮箱已经注册"}';
+        if($_POST['rtype'] == 1 || $_GET['rtype'] == 1){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $userType = $_POST['userType'];
+            if (empty($email) || empty($password)) {
+                echo '{"code":"-1","msg":"邮箱或者密码为空！"}';
+            }
+            $user = M("User");
+            $users = $user->where("email='%s' and status!=9999", array($email) )->select();
+            if (sizeof($users) == 1) {
+                echo '{"code":"-1","msg":"该邮箱已经注册"}';
 
-        } 
+            } 
 
-        $user = D('User','Service');
-        $users = $user->registerService();
-        
-        $display = $_GET['display'];
-        if ($display == 'json') {
-            dump($users);
-            echo json_encode($users);
-            exit();
-        }
-        if (sizeof($users) == 1) {
-            echo '{"code":"0","msg":""}';
+            $user = D('User','Service');
+            $users = $user->registerService();
+            
+            $display = $_GET['display'];
+            if ($display == 'json') {
+                dump($users);
+                echo json_encode($users);
+                exit();
+            }
+            if (sizeof($users) == 1) {
+                echo '{"code":"0","msg":""}';
 
-        } else {
-            echo '{"code":"-1","msg":"mysql error!"}';
+            } else {
+                echo '{"code":"-1","msg":"mysql error!"}';
+            }
+        }else {
+            $this->display();
         }
-        $this->display();
     }
 
     //修改密码
     public function changePassword(){
-        $email = $_POST['email'];
-        $mEmail = $_POST['mEmail'];
-        $pwd = $_POST['password'];
-        $newPwd = $_POST['newPassword'];
-        if ( empty($email) || empty($mEmail) || empty($pwd) || empty($newPwd) ) {
-            echo '{"code":"-1","msg":"邮箱或者新旧密码为空！"}';
-        }
-        if (!($mEmail == MD5($email."ENFENF"))) {
-            echo '{"code":"-1","msg":"登录信息错误"}';
-        }
+        if($_POST['rtype'] == 1 || $_GET['rtype'] == 1){
+            $email = $_POST['email'];
+            $mEmail = $_POST['mEmail'];
+            $pwd = $_POST['password'];
+            $newPwd = $_POST['newPassword'];
+            if ( empty($email) || empty($mEmail) || empty($pwd) || empty($newPwd) ) {
+                echo '{"code":"-1","msg":"邮箱或者新旧密码为空！"}';
+            }
+            if (!($mEmail == MD5($email."ENFENF"))) {
+                echo '{"code":"-1","msg":"登录信息错误"}';
+            }
 
-        $user = M('User');
-        $users = $user->where("email='%s' and password='%s' and status!=9999", array($email, $pwd))->select();
-        if(sizeof($users) == 0){
-            echo '{"code":"-1","msg":"原密码错误!"}';
-        }
+            $user = M('User');
+            $users = $user->where("email='%s' and password='%s' and status!=9999", array($email, $pwd))->select();
+            if(sizeof($users) == 0){
+                echo '{"code":"-1","msg":"原密码错误!"}';
+            }
 
-        $user->email = $email;
-        $user->password = $newPwd;
-        $user->save();
-        $this->display();
-        // $test_user = M('User');
-        // $test_user->id = $id;
-        // $test_user->password = $pwd;
-        // $test_user->save();
+            $user->email = $email;
+            $user->password = $newPwd;
+            $user->save();
+        }else{
+            $this->display();
+        }
     }
 
     //忘记密码时修改密码
