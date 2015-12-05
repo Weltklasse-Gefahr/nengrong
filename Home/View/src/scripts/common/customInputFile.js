8a783e936a703b3dc055f613d79874e99e6594e6
zhongqiu123 <472497810@qq.com>
2015-12-05 14:17:09 +0800

    relative url

--- Home/View/src/scripts/common/customInputFile.js
+++ Home/View/src/scripts/common/customInputFile.js
@@ -3,37 +3,59 @@ $(function($) {
 
 		$.each(this, function(i, item) {
 
-			var $this = $(this);
+			var $this = $(item);
 
 			if(!$this.is("input[type=file]")) {
 				return ;
 			}
+
+			var fileType, $inputFile, $preview
 			
-			if(!($this.parent().is(".input-file"))) {
-				$this.wrap($('<div class="input-file"></div>'));
-				$this.parent().before($('<div class="preview" style="display: none;"><img /><i class="del">x</i></div>'));
+			if(!($this.parent().is(".input-wrap"))) {
+
+				fileType = $(this).data("type") || "file";
+
+				if(fileType === "image") { // 图片预览
+					$this.wrap($('<div class="input-wrap input-' + fileType + '"></div>'));
+					$this.parent().before($('<div class="preview" style="display: none;"><a target="_blank" href="javascript:;"><img /></a><i class="del">x</i></div>'));
+				} else { // 文件展示文件名
+					$this.wrap($('<div class="input-wrap input-' + fileType + '">+</div>'));
+					$this.parent().after($('<div class="preview" style="display: none;"><a target="_blank" href="javascript:;"></a><i class="del">x</i></div>'));
+				}
 			}
+			$this.css("visibility", "visible");
 
 			var $inputFile = $this.parent(),
-				$preview = $inputFile.prev();
+				$preview = $inputFile.siblings(".preview");
 
 			$this.change(function(e) {
 
 				var	resultFile = this.files[0];
 
 				if(resultFile && resultFile.name) {
-					var reader = new FileReader();
+					// if(fileType === "image") {
+						var reader = new FileReader();
 
-					reader.onload = function (e) {
-	                    $preview.show().find("img").attr({
-	                    	src: this.result,
-	                    	alt: resultFile.name
-	                    });
+						reader.onload = function (e) {
+		                    var alink = $preview.show().find("a");
 
-	                    $inputFile.hide();
-	                };
+		                    if(fileType === "image") {
+		                    	alink.attr("href", this.result);
+			                    alink.find("img").attr({
+				                    src: this.result,
+				                    alt: resultFile.name
+				                });
+				                $inputFile.hide();
+			                } else {
+			                	alink.html(resultFile.name);
+			                }
+		                };
 
-	                reader.readAsDataURL(resultFile);
+		                reader.readAsDataURL(resultFile);
+		        	// } else {
+		        	// 	$preview.find("a")
+		        	// 	$inputFile.hide();
+		        	// }
 				}
 
 			});
@@ -41,6 +63,7 @@ $(function($) {
 			$preview.find(".del").click(function(e) {
 				$preview.hide();
 				$inputFile.show().find("input[type=file]").val("");
+				return false;
 			});
 		});
 	};
