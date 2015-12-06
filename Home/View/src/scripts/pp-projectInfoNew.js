6e95077d56d35baa67f3c60a8ccbefcbace1b602
zhongqiu123 <472497810@qq.com>
2015-12-07 01:35:31 +0800

    fix attachment

--- Home/View/src/scripts/pp-projectInfoNew.js
+++ Home/View/src/scripts/pp-projectInfoNew.js
@@ -1,13 +1,12 @@
 $(function() {
 
-	$(".l-nav").find(".myInformation").addClass("active")
-		.children("a").attr("href", "javascript:;");
+	$(".l-nav").find(".awaitingAssessment").addClass("active");
 
 	require("common/erqi/customUpload");
 	require("lib/jquery.form");
 	
 	// 上传图片
-	$(".detail.part2 .item input[type=file]").customUpload({
+	$(".detail.part1 input[type=file]").customUpload({
 		bg_url: "upload.png",
 		uploadType: "image",
 		width: "120px",
@@ -15,11 +14,12 @@ $(function() {
 	});
 
 	// 上传文件
-	$(".detail.part2 .finance input[type=file]").customUpload({
-		content: "+",
+	$(".detail.part2 input[type=file]").customUpload({
+		img_url: "attachment.png",
+		content: "上传附件",
 		uploadType: "file",
-		width: "20px",
-		height: "38px"
+		width: "80px",
+		height: "20px"
 	});
 
 	// 保存资料
@@ -27,10 +27,10 @@ $(function() {
 	   	// target: '#output',          //把服务器返回的内容放入id为output的元素中      
 	   	beforeSubmit: beforeSubmit, //提交前的回调函数  
 	   	success: successCallback,  	//提交后的回调函数
-	   	dataType: "json"           //html(默认), xml, script, json...接受服务端返回的类型  
+	   	dataType: "json",           //html(默认), xml, script, json...接受服务端返回的类型  
 	   	// clearForm: true,         //成功提交后，清除所有表单元素的值  
 	   	// resetForm: true,         //成功提交后，重置所有表单元素的值  
-	   	// timeout: 6000               //限制请求的时间，当请求大于3秒后，跳出请求
+	   	timeout: 6000               //限制请求的时间，当请求大于3秒后，跳出请求
 	};
 	  
 	function beforeSubmit(formData, jqForm, options){
@@ -66,4 +66,5 @@ $(function() {
 	}
 
 	$("#infoForm").ajaxForm(options);
+
 });
