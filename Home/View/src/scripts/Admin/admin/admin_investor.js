bd059d3116ec98892b30bfcf977e9076de4fc0a5
cebqby <122104615@qq.com>
2015-12-19 10:09:08 +0800

    管理平台代码

--- Home/View/src/scripts/Admin/admin/admin_investor.js
+++ Home/View/src/scripts/Admin/admin/admin_investor.js
@@ -8,8 +8,153 @@ $(function(){
 		});
 	});
 
-	$("#edit_id").click(function(){
+	$(".edit_id").click(function(){
 		var item_id = $(this).parent().parent().data("id");
-		location.href = "admin_investors_edit.html";
+		location.href="?c=Admin&a=getEditUserInfo&id="+item_id;
 	});
+
+	$("#edit_save").click(function(){ 
+		var edit_id= $.trim($("#edit_id").val());
+		var edit_email= $.trim($("#edit_email").val());
+		var edit_company_name= $.trim($("#edit_company_name").val());
+
+		if(!edit_email || !edit_company_name ) {
+			warning();
+			return ;
+		}
+
+		$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=changeProjectInvestorInfo" ,
+		    data: {
+				id: edit_id,
+		    	email: edit_email,
+		    	companyName: edit_company_name
+		    },
+			dataType: "json"
+		}).done(function(data){
+			if (data.code== 0)
+				{$("#myAlert_edit_success").show();}
+			else 
+				{$("#myAlert_edit_failed").show();}
+		});
+	});
+
+	$(".myAlert_edit_success_closed").click(function(){
+		$("#myAlert_edit_success").hide();
+	});
+
+	$(".myAlert_edit_failed_closed").click(function(){
+		$("#myAlert_edit_failed").hide();
+	});
+
+	$(".reset_id").click(function(){ 
+		var item_id = $(this).parent().parent().data("id");
+		$("#confirm_reset_id").off("click.reset");
+		$("#confirm_reset_id").on("click.reset", function(){
+			$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=resetPassword" ,
+		    data: {
+				id: item_id
+		    },
+			dataType: "json"
+			}).done(function(data){
+				if (data.code== 0)
+					{$("#myAlert_reset_success").show();}
+				else 
+					{$("#myAlert_reset_failed").show();}
+			});
+		});
+
+		
+	});
+
+	$(".myAlert_reset_success_closed").click(function(){
+		$("#myAlert_reset_success").hide();
+	});
+
+	$(".myAlert_reset_failed_closed").click(function(){
+		$("#myAlert_reset_failed").hide();
+	});
+
+	$(".delete_id").click(function(){ 
+		var item_id = $(this).parent().parent().data("id");
+		$("#confirm_delete_id").off("click.delete");
+		$("#confirm_delete_id").on("click.delete", function(){
+			$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=deleteUser" ,
+		    data: {
+				id: item_id
+		    },
+			dataType: "json"
+			}).done(function(data){
+				if (data.code== 0)
+					{
+						$("#myAlert_delete_success").show();
+					}
+				else 
+					{
+						$("#myAlert_delete_failed").show();
+					}
+			});
+		});
+
+		
+	});
+
+	$(".myAlert_delete_success_closed").click(function(){
+		$("#myAlert_delete_success").hide();
+		location.href="?c=Admin&a=getAllProjectInvestorInfo";
+	});
+
+	$(".myAlert_delete_failed_closed").click(function(){
+		$("#myAlert_delete_failed").hide();
+		location.href="?c=Admin&a=getAllProjectInvestorInfo";
+	});
+
+
+	$("#add_investor").click(function(){ 
+		var email= $.trim($("#add_mail").val());
+		var company_name= $.trim($("#add_company_name").val());
+
+		if(!email || !company_name) {
+			warning();
+			return ;
+		}
+
+		$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=addProjectInvestor" ,
+		    data: {
+		    	email: email,
+		    	companyName: company_name
+		    },
+			dataType: "json"
+		}).done(function(data){
+			if (data.code== 0)
+				{
+					$("#modal-add-event").hide();
+					$("#myAlert_add_success").show();
+				}
+			else 
+				{
+					$("#myAlert_add_failed").show();
+				}
+		});
+
+		
+	});
+
+	$(".myAlert_add_success_closed").click(function(){
+		$("#myAlert_add_success").hide();
+		location.href="?c=Admin&a=getAllProjectInvestorInfo";
+	});
+
+	$(".myAlert_add_failed_closed").click(function(){
+		$("#myAlert_add_failed").hide();
+		location.href="?c=Admin&a=getAllProjectInvestorInfo";
+	});
+
 });
--- Home/View/src/scripts/Admin/admin/admin_provider.js
+++ Home/View/src/scripts/Admin/admin/admin_provider.js
@@ -8,8 +8,112 @@ $(function(){
 		});
 	});
 
-	$("#edit_id").click(function(){
+	$(".edit_id").click(function(){
 		var item_id = $(this).parent().parent().data("id");
-		location.href = "admin_provider_edit.html";
+		location.href="?c=Admin&a=getEditUserInfo&id="+item_id;
 	});
+
+	$("#edit_save").click(function(){ 
+		var edit_id= $.trim($("#edit_id").val());
+		var edit_email= $.trim($("#edit_email").val());
+		var edit_company_phone= $.trim($("#edit_company_phone").val());
+		var edit_company_telephone= $.trim($("#edit_company_telephone").val());
+
+		if(!edit_email || !edit_company_phone || !edit_company_telephone ) {
+			warning();
+			return ;
+		}
+
+		$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=changeProjectProviderInfo" ,
+		    data: {
+				id: edit_id,
+		    	email: edit_email,
+		    	phone: edit_company_phone,
+		    	telephone:edit_company_telephone
+		    },
+			dataType: "json"
+		}).done(function(data){
+			if (data.code== 0)
+				{$("#myAlert_edit_success").show();}
+			else 
+				{$("#myAlert_edit_failed").show();}
+		});
+	});
+
+	$(".myAlert_edit_success_closed").click(function(){
+		$("#myAlert_edit_success").hide();
+	});
+
+	$(".myAlert_edit_failed_closed").click(function(){
+		$("#myAlert_edit_failed").hide();
+	});
+
+	$(".reset_id").click(function(){ 
+		var item_id = $(this).parent().parent().data("id");
+		$("#confirm_reset_id").off("click.reset");
+		$("#confirm_reset_id").on("click.reset", function(){
+			$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=resetPassword" ,
+		    data: {
+				id: item_id
+		    },
+			dataType: "json"
+			}).done(function(data){
+				if (data.code== 0)
+					{$("#myAlert_reset_success").show();}
+				else 
+					{$("#myAlert_reset_failed").show();}
+			});
+		});
+
+		
+	});
+
+	$(".myAlert_reset_success_closed").click(function(){
+		$("#myAlert_reset_success").hide();
+	});
+
+	$(".myAlert_reset_failed_closed").click(function(){
+		$("#myAlert_reset_failed").hide();
+	});
+
+	$(".delete_id").click(function(){ 
+		var item_id = $(this).parent().parent().data("id");
+		$("#confirm_delete_id").off("click.delete");
+		$("#confirm_delete_id").on("click.delete", function(){
+			$.ajax({
+		    type: "post",
+		    url: "?c=Admin&a=deleteUser" ,
+		    data: {
+				id: item_id
+		    },
+			dataType: "json"
+			}).done(function(data){
+				if (data.code== 0)
+					{
+						$("#myAlert_delete_success").show();
+					}
+				else 
+					{
+						$("#myAlert_delete_failed").show();
+					}
+			});
+		});
+
+		
+	});
+
+	$(".myAlert_delete_success_closed").click(function(){
+		$("#myAlert_delete_success").hide();
+		location.href="?c=Admin&a=getAllProjectProviderInfo";
+	});
+
+	$(".myAlert_delete_failed_closed").click(function(){
+		$("#myAlert_delete_failed").hide();
+		location.href="?c=Admin&a=getAllProjectProviderInfo";
+	});
+
 });
