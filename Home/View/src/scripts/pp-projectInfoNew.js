e0dc724d69a6b5dd469c6e415fe9654ad80d22bb
zhongqiu123 <472497810@qq.com>
2015-12-20 18:11:21 +0800

    submit control

--- Home/View/src/scripts/pp-projectInfoNew.js
+++ Home/View/src/scripts/pp-projectInfoNew.js
@@ -11,9 +11,10 @@ $(function() {
 
 		$(this).addClass("active").siblings().removeClass("active");
 		$(this).siblings("input").val($(this).data("filter"));
-		
+		$form.find("li:hidden input, li:hidden select").prop("disabled", false);
+
 		$("#infoForm").attr("class", [
-			["housetop", "ground", "ground"][$("input[name=project_type]").val()-1],
+			["housetop", "ground", "bigground"][$("input[name=project_type]").val()-1],
 			["nonBuild", "build"][$("input[name=build_state]").val()-1]
 		].join("_"));
 	});
@@ -87,7 +88,12 @@ $(function() {
 
 	// 日期选择框
 	require("lib/jquery-ui");
-	$("input[data-type=date]").datepicker();
+	$.datepicker.regional["zh-CN"] = { closeText: "关闭", prevText: "上月", nextText: "下月", currentText: "今天", monthNames: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"], monthNamesShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"], dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"], dayNamesShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"], dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"], weekHeader: "周", dateFormat: "yy-mm-dd", firstDay: 1, isRTL: !1, showMonthAfterYear: !0, yearSuffix: "年" };
+	$.datepicker.setDefaults($.datepicker.regional['zh-CN']);
+	$("input[data-type=date]").datepicker({
+		changeMonth: true,
+      	changeYear: true
+	});
 
 	// 保存资料
 	var options = {
@@ -144,6 +150,7 @@ $(function() {
 			return false;
 		} else {
 			$form.find("[name=optype]").val(optype);
+			$form.find("li:hidden input, li:hidden select").prop("disabled", true);
 			return true;
 		}
 	});
