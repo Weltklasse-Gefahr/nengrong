671fbad43001040f38b4d8e6ebed8d9705e0aa95
qianqiang <18953197047@189.cn>
2015-12-23 21:07:45 +0800

    尽职调查后端

--- Home/Service/ProjectService.class.php
+++ Home/Service/ProjectService.class.php
@@ -33,4 +33,53 @@ class ProjectService extends Model{
             return $groundInfo[0];
         }
     }
+
+    /**
+    **@auth qianqiang
+    **@breif 尽职调查保存时，保存项目信息
+    **@return 保存成功返回true，失败返回false
+    **@date 2015.12.23
+    **/ 
+    public function saveHousetopProject($proData){
+        $housetop = M("Housetop");
+        if($this->hasSaveHousetopProject($proData['projectId']) == 1){
+            $housetop->where("project_id='".$proData['projectId']."' and status=51")->save($proData);
+        }else{
+            $proData['status'] = 51;
+            $proData['create_date'] = date("Y-m-d H:i:s",time());
+            $housetop->add($proData);
+        }
+        $housetopInfo = $housetop->where($proData)->where("status=51")->select();
+        if(sizeof($housetopInfo) == 1)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+    **@auth qianqiang
+    **@breif 尽职调查提交时，提交项目信息
+    **@return 提交成功返回true，失败返回false
+    **@date 2015.12.23
+    **/ 
+    public function submitHousetopProject($proData){
+        //保存记录，如果有save数据，进行删除
+    }
+
+    /**
+    **@auth qianqiang
+    **@breif 判断是否有保存的屋顶项目
+    **@return 存在返回true，不存在返回false
+    **@date 2015.12.23
+    **/ 
+    public function hasSaveHousetopProject($projectId){
+        $objProject = D("Housetop");
+        $condition["project_id"] = $projectId;
+        $condition["status"] = 51;
+        $proInfo = $objProject->where($condition)->select();
+        if(sizeof($proInfo) == 1)
+            return true;
+        else
+            return false;
+    }
 }
