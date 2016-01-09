0375d6026ede909c6a5d449664db44fcb9e3dbd8
qianqiang <18953197047@189.cn>
2016-01-09 13:12:53 +0800

    qianqiang

--- Home/Controller/InnerStaffController.class.php
+++ Home/Controller/InnerStaffController.class.php
@@ -82,11 +82,11 @@ class InnerStaffController extends Controller {
             $doc = D("Doc", "Service");
             $docInfo = $doc->uploadFileAndPictrue($docFile, $docFile);
             if(!empty($docInfo)){
-                if(isset($_SESSION['doc_mul']))
-                    $_SESSION['doc_mul'] = $_SESSION['doc_mul'].",".$docInfo['attachment'];
-                else
-                    $_SESSION['doc_mul'] = $docInfo['attachment'];
-                echo $_SESSION['doc_mul'];
+                // if(isset($_SESSION['doc_mul']))
+                //     $_SESSION['doc_mul'] = $_SESSION['doc_mul'].",".$docInfo['attachment'];
+                // else
+                //     $_SESSION['doc_mul'] = $docInfo['attachment'];
+                // echo $_SESSION['doc_mul'];
                 echo '{"code":"0","msg":"success","id":"'.$docInfo['attachment'].'"}';
             }else{
                 echo '{"code":"-1","msg":"上传失败！"}';
@@ -141,7 +141,7 @@ class InnerStaffController extends Controller {
             $evaData['project_quality_situation'] = $_POST['project_quality_situation'];
             $evaData['project_invest_situation'] = $_POST['project_invest_situation'];
             $evaData['project_earnings_situation'] = $_POST['project_earnings_situation'];
-            $evaData['doc_mul'] = $_SESSION['doc_mul'];
+            $evaData['doc_mul'] = $_POST['doc_mul'];
 // echo "start   :";dump($proData);dump($evaData);exit;
             $res = $objProject->saveHousetopProject($proData);
             if($res == true){
@@ -205,7 +205,7 @@ class InnerStaffController extends Controller {
             $evaData['project_quality_situation'] = $_POST['project_quality_situation'];
             $evaData['project_invest_situation'] = $_POST['project_invest_situation'];
             $evaData['project_earnings_situation'] = $_POST['project_earnings_situation'];
-            $evaData['doc_mul'] = $_SESSION['doc_mul'];
+            $evaData['doc_mul'] = $_POST['doc_mul'];
 
 			$res = $objProject->submitHousetopProject($proData);
             if($res == true){
