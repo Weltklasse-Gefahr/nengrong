c7f209369b021593aebda2310d8b8d26279008dc
cebqby <122104615@qq.com>
2016-01-11 21:26:46 +0800

    “推荐项目”

--- Home/View/src/scripts/PIrecommendedProject.js
+++ Home/View/src/scripts/PIrecommendedProject.js
@@ -1,8 +1,16 @@
 $(function() {
 
-	$(".l-nav").find(".recommendedProject").addClass("active")
-		.children("a").attr("href", "javascript:;");
+	$(".l-nav").find(".recommendedProject").addClass("active");
 
 	require("common/erqi/list-opt.js");
 	require("common/erqi/pager.js");
+
+	/* ·ÖÒ³ */
+	$(".pager a").click(function() {
+		var $this = $(this);
+		if(!$this.hasClass("active")) {
+			location.href = "?c=ProjectInvestorMyPro&a=recommendedProject&page="+$this.data("pageno");
+		}
+		return false;
+	});
 });
--- Home/View/src/scripts/pp-agreementProject.js
+++ Home/View/src/scripts/pp-agreementProject.js
@@ -5,4 +5,14 @@ $(function() {
 
 	require("common/erqi/list-opt.js");
 	require("common/erqi/pager.js");
+
+	/* ·ÖÒ³ */
+	$(".pager a").click(function() {
+		var $this = $(this);
+		if(!$this.hasClass("active")) {
+			location.href = "?c=ProjectProviderMyPro&a=agreementProject&page="+$this.data("pageno");
+		}
+		return false;
+	});
+
 });
