6dff295b4b45fd8dfad75def889297c565d3c641
qiujinhangit <qiujinhan@gmail.com>
2016-01-17 19:33:19 +0800

    d

--- Home/Controller/InnerStaffController.class.php
+++ Home/Controller/InnerStaffController.class.php
@@ -10,15 +10,68 @@ class InnerStaffController extends Controller {
     **@auth qiujinhan
     **@breif 在客服中导出一个项目的信息
     **@date 2016.1.17
+    **@参数 ?c=InnerStaff&a=export&no=sss&token=xxxx
     **/
     public function export()
     {
-        
+        //echo jj;exit;
+        /*
         isLogin($_COOKIE['email'],$_COOKIE['mEmail']);
         $projectCode  = $_POST['no']     ? $_POST['no']:$_GET['no'];
         $mProjectCode = $_POST['token']  ? $_POST['token']:$_GET['token'];
         isProjectCodeRight($projectCode, $mProjectCode); 
-        
+        $getJsonFlag = 1;
+        //获取项目投资方信息
+        $data = $this->getProjectProviderInfo($projectCode, null, $getJsonFlag);
+        */
+        import("Org.Util.PHPExcel");
+        $obpe = new \PHPExcel();
+        $obpe_pro = $obpe->getProperties();
+        $obpe_pro->setCreator('midoks')//设置创建者
+             ->setLastModifiedBy('2013/2/16 15:00')//设置时间
+             ->setTitle('data')//设置标题
+             ->setSubject('beizhu')//设置备注
+             ->setDescription('miaoshu')//设置描述
+             ->setKeywords('keyword')//设置关键字 | 标记
+             ->setCategory('catagory');//设置类别
+            //设置当前sheet索引,用于后续的内容操作
+            //一般用在对个Sheet的时候才需要显示调用
+            //缺省情况下,PHPExcel会自动创建第一个SHEET被设置SheetIndex=0
+            //设置SHEET
+            $obpe->setactivesheetindex(0);
+            //写入多行数据
+            //模拟数据
+            $mulit_arr = array(
+                array('标题1', '标题2', '标题3'),
+                array('a', 'b', 'c'),
+                array('d', 'e', 'f')
+            );
+                       
+            //创建一个新的工作空间(sheet)
+            $obpe->createSheet();
+            $obpe->setactivesheetindex(1);
+            //写入多行数据
+            foreach($mulit_arr as $k=>$v){
+                $k = $k+1;
+                /* @func 设置列 */
+                $obpe->getactivesheet()->setcellvalue('A'.$k, $v[0]);
+                $obpe->getactivesheet()->setcellvalue('B'.$k, $v[1]);
+                $obpe->getactivesheet()->setcellvalue('C'.$k, $v[2]);
+            }
+            import("Org.Util.PHPExcel.IOFactory");
+            $objWriter = \PHPExcel_IOFactory::CreateWriter($obpe,"Excel2007");
+            //$objWriter->save('qiujinhan.xls');exit;
+            header('Pragma: public');
+            header('Expires: 0');
+            header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
+            header('Content-Type:application/force-download');
+            header('Content-Type:application/vnd.ms-execl');
+            header('Content-Type:application/octet-stream');
+            header('Content-Type:application/download');
+            header("Content-Disposition:attachment;filename='qiujinhan.xls'");
+            header('Content-Transfer-Encoding:binary');
+            $objWriter->save('php://output');
+
     }
 
     /**
