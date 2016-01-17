95b3d4a6ea4616e89b9f999a1e2e5c7bfeb898d8
zhongqiu123 <472497810@qq.com>
2016-01-17 19:33:14 +0800

    fix

--- Home/View/src/scripts/common/erqi/l-nav.js
+++ Home/View/src/scripts/common/erqi/l-nav.js
@@ -1,4 +1,4 @@
-$(".l-nav dl a").each(function() {
+$(".l-nav dl a").add(".l-nav .export, .l-nav .del").each(function() {
 	var param = $.parseQueryParam();
 	if(param.no || param.token) {
 		$(this).attr("href", $(this).attr("href")+"&no="+param.no+"&token="+param.token);
