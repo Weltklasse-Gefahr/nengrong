219cb14f259b9c88557763993ab48539612626cb
zhongqiu123 <472497810@qq.com>
2016-01-23 19:44:12 +0800

    add 校验

--- Home/View/src/scripts/pp-projectInfoNew.js
+++ Home/View/src/scripts/pp-projectInfoNew.js
@@ -173,14 +173,115 @@ $(function() {
 			"city": "required",
 			"county": "required",
    			"project_address": "required",
-   			"housetop_owner": "required"
+   			"housetop_owner": "required",
+   			"company_capital": "required",
+   			"electricity_total": "required",
+   			"electricity_pay": "required",
+
+   			"housetop_type_other": {
+   				"required": function() {
+   					if($('[name="housetop_type"]').is(":visible") && $('[name="housetop_type"]').val() == "0") {
+   						return true;
+   					}
+   					return false;
+	   			}
+	   		},
+   			"housetop_area": "required",
+   			"housetop_waterproof_time": "required",
+   			"housetop_age": "required",
+   			"housetop_direction_other": {
+   				"required": function() {
+   					if($('[name="housetop_direction"]').is(":visible") && $('[name="housetop_direction"]').val() == "0") {
+   						return true;
+   					}
+   					return false;
+	   			}
+	   		},
+	   		"housetop_load": "required",
+
+	   		"ground_property_other":  {
+   				"required": function() {
+   					if($('[name="ground_property"]').is(":visible") && $('[name="ground_property"]').val() == "0") {
+   						return true;
+   					}
+   					return false;
+	   			}
+	   		},
+	   		"ground_area": "required",
+	   		"rent_time": "required",
+	   		"rent_pay": "required",
+	   		"control_room_area": "required",
+	   		"sell_sum": "required",
+
+	   		"transformer_capacity": "required",
+	   		"voltage_level": "required",
+	   		"electricity_distance": "required",
+
+	   		"company_invest": "required",
+	   		"company_EPC": "required",
+	   		"capacity_level": "required",
+
+	   		"synchronize_date": {
+	   			"required": true,
+	   			"dateISO": true
+	   		},
+	   		"electricity_data": {
+	   			"required": true,
+	   			"number": true,
+	   			"min": 0
+	   		},
+	   		"plan_financing": {
+	   			"required": true,
+	   			"number": true,
+	   			"min": 0
+	   		}
    		},
    		messages: {
    			"province": "请选择省份",
    			"city": "请选择市",
 			"county": "请选择区",
 			"project_address": "请填写详细地址",
-			"housetop_owner": "请填写屋顶业主名称"
+			"housetop_owner": "请填写屋顶业主名称",
+			"company_capital": "请填写注册资本金",
+   			"electricity_total": "请填写年用电量",
+   			"electricity_pay": "请填写电费",
+
+   			"housetop_type_other": "请填写屋顶类型",
+   			"housetop_area": "请填写屋顶面积",
+   			"housetop_waterproof_time": "请填写屋顶防水周期",
+   			"housetop_age": "请填写屋顶使用寿命",
+   			"housetop_direction_other": "请填写屋顶朝向",
+   			"housetop_load": "请填写屋顶活载荷",
+
+   			"ground_property_other": "请填写土地性质",
+   			"ground_area": "请填写租赁土地面积",
+   			"rent_time": "请填写租赁年限",
+   			"rent_pay": "请填写租赁租金",
+	   		"control_room_area": "请填写中控室建筑面积",
+	   		"sell_sum": "请填写出让金额",
+
+	   		"transformer_capacity": "请填写上级变压器容量",
+	   		"voltage_level": "请填写并网电压等级",
+	   		"electricity_distance": "请填写电网接入点距离",
+
+	   		"company_invest": "请填写单位投资",
+	   		"company_EPC": "请填写EPC厂家",
+	   		"capacity_level": "请填写资质等级",
+
+	   		"synchronize_date": {
+	   			"required": "请填写并网时间",
+	   			"dateISO": "并网时间输入格式不对"
+	   		},
+	   		"electricity_data": {
+	   			"required": "请填写历史发电量数据",
+	   			"number": "历史发电量数据应为数字",
+	   			"min": "历史发电量数据应大于0"
+	   		},
+	   		"plan_financing": {
+	   			"required": "请填写拟融资金额",
+	   			"number": "拟融资金额应为数字",
+	   			"min": "拟融资金额应大于0"
+	   		}
    		},
    		errorClass: 'validate-error',
    		focusInvalid: false,
@@ -199,10 +300,97 @@ $(function() {
 	});
 
 	function beforeSubmit(formData, jqForm, options) {
-		var state = jqForm.attr("class").match(/(housetop_nonBuild|/)
-		if() {
+		var match = jqForm.attr("class").match(/(housetop_nonBuild|housetop_build|ground_nonBuild|ground_build)/),
+			state = match && match[1];
 
+		// 二次拦截校验
+		switch(state) {
+			case "housetop_nonBuild":
+			case "housetop_build":
+				var $picture_full = $('.housetop_nonBuild_item [name=picture_full]'),
+					$picture_full_hiddenId = $('.housetop_nonBuild_item [name=picture_full_hiddenId]');
+				if(!$picture_full.val() && !$picture_full_hiddenId.val()) {
+					alert("请上传屋顶全景图");
+					$picture_full.focus();
+					return false;
+				}
+
+				var $picture_south = $('.housetop_nonBuild_item [name=picture_south]'),
+					$picture_south_hiddenId = $('.housetop_nonBuild_item [name=picture_south_hiddenId]');
+				if(!$picture_south.val() && !$picture_south_hiddenId.val()) {
+					alert("请上传屋顶正南向照片");
+					$picture_south.focus();
+					return false;
+				}
+				break;
+			case "ground_nonBuild":
+			case "ground_build":
+				var $picture_full = $('.ground_nonBuild_item [name=picture_full]'),
+					$picture_full_hiddenId = $('.ground_nonBuild_item [name=picture_full_hiddenId]');
+				if(!$picture_full.val() && !$picture_full_hiddenId.val()) {
+					alert("请上传场地情况全景图");
+					$picture_full.focus();
+					return false;
+				}
+
+				var $picture_field = $('.ground_nonBuild_item [name=picture_field]'),
+					$picture_field_hiddenId = $('.ground_nonBuild_item [name=picture_field_hiddenId]');
+				if(!$picture_field.val() && !$picture_field_hiddenId.val()) {
+					alert("请上传场平照片");
+					$picture_field.focus();
+					return false;
+				}
+
+				var $picture_transformer = $('.ground_nonBuild_item [name=picture_transformer]'),
+					$picture_transformer_hiddenId = $('.ground_nonBuild_item [name=picture_transformer_hiddenId]');
+				if(!$picture_transformer.val() && !$picture_transformer_hiddenId.val()) {
+					alert("请上传变电站照片");
+					$picture_transformer.focus();
+					return false;
+				}
+				break;
 		}
+
+		switch(state) {
+			case "housetop_nonBuild":
+			case "ground_nonBuild":
+				var $plan_build_volume = $('.housetop_nonBuild_item [name=plan_build_volume]');
+				if(!$plan_build_volume.val()) {
+					alert("请填写拟建设容量");
+					$plan_build_volume.focus();
+					return false;
+				}
+
+				var $cooperation_type = $('.housetop_nonBuild_item [name=cooperation_type]');
+				if(!$cooperation_type.filter(":checked").length) {
+					alert("与能融网合作方式至少需选中一项");
+					$cooperation_type.focus();
+					return false;
+				}
+				break;
+			case "housetop_build":
+			case "ground_build":
+				var $plan_build_volume = $('.housetop_build_item [name=plan_build_volume]');
+				if(!$plan_build_volume.val()) {
+					alert("请填写建设容量");
+					$plan_build_volume.focus();
+					return false;
+				}
+
+				var $cooperation_type = $('.housetop_build_item [name=cooperation_type]');
+				if(!$cooperation_type.filter(":checked").length) {
+					alert("与能融网合作方式至少需选中一项");
+					$cooperation_type.focus();
+					return false;
+				}
+				break;
+		}
+
+		$form.find("li:hidden input, li:hidden select").prop("disabled", true);
+		$form.find('[data-type="mul"]').each(function() {
+			$(this).attr("name", $(this).attr("name").replace(/^(.*)$/, "$1[]"));
+		});
+
 	   	return true;
 	}
 
@@ -247,14 +435,9 @@ $(function() {
 			});
 			
 			return false;
-		} else {
-			$form.find("[name=optype]").val(optype);
-			$form.find("li:hidden input, li:hidden select").prop("disabled", true);
-			$form.find('[data-type="mul"]').each(function() {
-				$(this).attr("name", $(this).attr("name").replace(/^(.*)$/, "$1[]"));
-			});
-			return true;
 		}
+		$form.find("[name=optype]").val(optype);
+		return true;
 	});
 
 	// $form.ajaxForm(options);
