19282b12429d519b22de47bf63be1e7e3d8ba7be
zhongqiu123 <472497810@qq.com>
2016-01-27 23:07:00 +0800

    投资方导出按钮

--- Home/View/src/scripts/common/erqi/l-nav.js
+++ Home/View/src/scripts/common/erqi/l-nav.js
@@ -1,6 +1,18 @@
-$(".l-nav-is dl a").add(".l-nav-is .export, .l-nav-is .del").each(function() {
+$(function() {
 	var param = $.parseQueryParam();
-	if(param.no || param.token) {
-		$(this).attr("href", $(this).attr("href")+"&no="+param.no+"&token="+param.token);
-	}
+
+	// 维护客服左边栏no和token
+	$(".l-nav-is dl a").add(".l-nav-is .export, .l-nav-is .del").each(function() {
+		if(param.no || param.token) {
+			$(this).attr("href", $(this).attr("href")+"&no="+param.no+"&token="+param.token);
+		}
+	});
+
+	// 维护项目投资方，导出按钮no和token
+	$(".r-content .btn.export").each(function() {
+		if(param.no || param.token) {
+			$(this).attr("href", $(this).attr("href")+"&no="+param.no+"&token="+param.token);
+		}
+	})
 });
+
