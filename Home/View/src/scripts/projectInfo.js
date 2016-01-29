daf76446a1ce8b22cd114fc9ddef43cf56ff7021
github <github>
2016-01-29 21:39:13 +0800

    new prj

--- Home/View/src/scripts/projectInfo.js
+++ Home/View/src/scripts/projectInfo.js
@@ -1,3 +1,17 @@
 $(function(){
 	$(".l-nav").find(".projectInfo").addClass("active");
+
+	$("#commentbtn").click(function(){ 
+		var commenttexval= $.trim($("#commenttex").val());
+
+		$.ajax({
+		    type: "post",
+		    url: "?c=user&a=projectInfo" ,
+		    data: {	
+		    	comment: commenttexval,
+		    	rtype: 1
+		    },
+			dataType: "json"
+		}).done(function(data){});
+	});
 });
