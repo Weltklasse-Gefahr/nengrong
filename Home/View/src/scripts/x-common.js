846f4c41dca71f781f02fec2e813c05a1b029c58
zhongqiu123 <472497810@qq.com>
2016-01-30 18:58:05 +0800

    项目提供方-我的资料校验

--- Home/View/src/scripts/x-common.js
+++ Home/View/src/scripts/x-common.js
@@ -1,6 +1,7 @@
 require("lib/jquery.min.js");
 require("lib/jquery-validation-1.14.0/core.js");
 require("lib/jquery-validation-1.14.0/localization/messages_zh.js");
+require("lib/jquery-validation-1.14.0/additional/mobile.js");
 require("common/erqi/dialog.js");
 require("common/addFavorite.js");
 require("common/erqi/header.js"); // 登出，设置公司名称等
