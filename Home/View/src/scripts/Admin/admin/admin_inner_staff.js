6a8f2361c033f82f64d76672953da61897171249
cebqby <122104615@qq.com>
2016-01-31 20:13:29 +0800

    ddd

--- Home/View/src/scripts/Admin/admin/admin_inner_staff.js
+++ Home/View/src/scripts/Admin/admin/admin_inner_staff.js
@@ -8,7 +8,7 @@ $(function(){
 		});
 	});
 
-	$(".edit_id").click(function(){
+	$("#DataTables_Table_0").on("click", ".edit_id", function(){
 		var item_id = $(this).parent().parent().data("id");
 		location.href="?c=Admin&a=getEditUserInfo&id="+item_id;
 	});
@@ -49,8 +49,8 @@ $(function(){
 	$(".myAlert_edit_failed_closed").click(function(){
 		$("#myAlert_edit_failed").hide();
 	});
-
-	$(".reset_id").click(function(){ 
+	
+	$("#DataTables_Table_0").on("click", ".reset_id", function(){ 
 		var item_id = $(this).parent().parent().data("id");
 		$("#confirm_reset_id").off("click.reset");
 		$("#confirm_reset_id").on("click.reset", function(){
@@ -80,7 +80,7 @@ $(function(){
 		$("#myAlert_reset_failed").hide();
 	});
 
-	$(".delete_id").click(function(){ 
+	$("#DataTables_Table_0").on("click", ".delete_id", function(){ 
 		var item_id = $(this).parent().parent().data("id");
 		$("#confirm_delete_id").off("click.delete");
 		$("#confirm_delete_id").on("click.delete", function(){
